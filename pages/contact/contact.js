82eea75ff83aa85aa68571398bbba30862fcefc1
samliu <html5@qq.com>
2017-08-24 18:09:40 +0800

    no message

---
+++ pages/contact/contact.js
@@ -0,0 +1,93 @@
+// contact.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    latitude: 28.231280,
+    longitude: 112.882540,
+      markers: [{
+        latitude: 28.231280,
+        longitude: 112.882540,
+        name: '长沙中电软件园'
+      }],
+      covers: [{
+        latitude: 28.231280,
+        longitude: 112.882540,
+      }, {
+          latitude: 28.231280,
+          longitude: 112.882540,
+        rotate: 180
+      }]
+  },
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  },
+  callmeTap: function () {
+    wx.makePhoneCall({
+      phoneNumber: '13507425295'
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+
+})
---
+++ pages/index/index.js
@@ -0,0 +1,102 @@
+var app = getApp();
+ Page({
+  data: {
+    img_data: [
+      {
+        "title": "1",
+        "img_url": "../../images/b1.jpg",
+      },
+      {
+        "title": "2",
+        "img_url": "../../images/b2.jpg"
+      },
+      {
+        "title": "3",
+        "img_url": "../../images/b3.jpg"
+      },
+      {
+        "title": "4",
+        "img_url": "../../images/b4.jpg"
+      },
+      {
+        "title": "5",
+        "img_url": "../../images/b5.jpg"
+      }     
+    ],
+    nav: [
+      {
+        "imgurl": "../../images/web.png",
+        "navigator": "/about/about",
+        "title": "企业网站制作"
+      },
+      {
+        "imgurl": "../../images/weapp.png",
+        "navigator": "/about/about",
+        "title": "小程序开发"
+      },
+      {
+        "imgurl": "../../images/mb.png",
+        "navigator": "/contact",
+        "title": "模板网站制作"
+      },
+      {
+        "imgurl": "../../images/seo.png",
+        "navigator": "../pages/about",
+        "title": "网站SEO优化"
+      }
+    ],
+    caselist:[
+      {
+        "imgurl": "../../images/t1.png",
+        "navigator": "../about",
+        "mode": 'aspectFit',
+        "title": "企业网站制作"
+      },
+      {
+        "imgurl": "../../images/t1.png",
+        "navigator": "../pages/about",
+        "mode": 'aspectFit',
+        "title": "小程序开发"
+      }
+      ,
+      {
+        "imgurl": "../../images/t1.png",
+        "navigator": "../pages/about",
+        "mode": 'aspectFit',
+        "title": "小程序开发"
+      }
+      ,
+      {
+        "imgurl": "../../images/t1.png",
+        "navigator": "../pages/about",
+        "mode": 'aspectFit',
+        "title": "小程序开发"
+      }
+    ],
+      suggests: [
+      {
+        "name": "高端集团网站",
+        "avatar": "../../images/t1.png",
+        "tags": [
+          "PC版",
+          "手机版",
+          "微信公众号"
+        ],
+        "text": "适合大型集团公司品牌推广"
+      }
+    ],
+    is_dots: true,
+    swiper_data_num: 0,
+    swiperCurrent: 0,
+    swiper_style: 1
+  },
+
+  onLoad: function () {
+    this.setData({ swiper_data_num: this.data.img_data.length });
+  },
+  swiperChange: function (e) {
+    this.setData({ swiperCurrent: e.detail.current });
+  },
+  //改变轮播图样式
+
+})
